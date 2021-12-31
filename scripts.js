013503297250b99ed89f5919eaa50a8bb55fc589
laqmarc <laqmarc@gmail.com>
2021-12-31 17:27:04 +0100

    inici

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+//open menu function
+/*
+function obrirMenu(){
+    var element = document.getElementById("menuObert");
+    element.classList.add("obretsesam");
+}
+
+function tancarMenu(){
+    var element = document.getElementById("menuObert");
+    element.classList.remove("obretsesam");
+}*/
 
